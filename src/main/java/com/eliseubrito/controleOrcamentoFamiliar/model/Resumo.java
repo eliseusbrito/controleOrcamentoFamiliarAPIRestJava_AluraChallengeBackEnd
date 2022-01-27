05f23a85f52a8a49d12fe53d816e46a0171db503
eliseusbrito <eliseusbrito@gmail.com>
2022-01-27 08:53:58 -0300

    Adicionado ao resumo do mês os totais de gastos por categoria

--- src/main/java/com/eliseubrito/controleOrcamentoFamiliar/model/Resumo.java
+++ src/main/java/com/eliseubrito/controleOrcamentoFamiliar/model/Resumo.java
@@ -12,5 +12,13 @@ public class Resumo {
     private double totalReceitas;
     private double totalDespesas;
     private double saldoFinalMes;
+    private double totalDespesasAlimentação;
+    private double totalDespesasSaude;
+    private double totalDespesasMoradia;
+    private double totalDespesasTransporte;
+    private double totalDespesasEducacao;
+    private double totalDespesasLazer;
+    private double totalDespesasImprevistos;
+    private double totalDespesasOutras;
 
 }
--- src/main/java/com/eliseubrito/controleOrcamentoFamiliar/service/ResumoService.java
+++ src/main/java/com/eliseubrito/controleOrcamentoFamiliar/service/ResumoService.java
@@ -1,5 +1,6 @@
 package com.eliseubrito.controleOrcamentoFamiliar.service;
 
+import com.eliseubrito.controleOrcamentoFamiliar.enuns.Categoria;
 import com.eliseubrito.controleOrcamentoFamiliar.model.Despesa;
 import com.eliseubrito.controleOrcamentoFamiliar.model.Receita;
 import com.eliseubrito.controleOrcamentoFamiliar.model.Resumo;
@@ -26,13 +27,46 @@ public class ResumoService {
     public Resumo findByMes(Integer ano, Integer mes) {
         LocalDateTime startDate = LocalDateTime.now().with(LocalDateTime.of(LocalDate.MIN, LocalTime.MIN)).withMonth(mes).withYear(ano).with(TemporalAdjusters.firstDayOfMonth());
         LocalDateTime endDate = LocalDateTime.now().with(LocalDateTime.of(LocalDate.MAX, LocalTime.MAX)).withMonth(mes).withYear(ano).with(TemporalAdjusters.lastDayOfMonth());
+        Resumo resumo = new Resumo();
 
         List<Despesa> despesas = despesaRepository.findByAnoAndMes(startDate, endDate);
         double totalDespesas = 0;
         for (int i = 0; i < despesas.size(); i++) {
             totalDespesas = despesas.get(i).getValor() + totalDespesas;
+            if (despesas.get(i).getCategoria() == Categoria.ALIMENTAÇÃO) {
+                double totalDespesasAlimentacao = 0;
+                resumo.setTotalDespesasAlimentação(despesas.get(i).getValor() + totalDespesasAlimentacao);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.SAÚDE) {
+                double totalDespesasSaude = 0;
+                resumo.setTotalDespesasSaude(despesas.get(i).getValor() + totalDespesasSaude);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.MORADIA) {
+                double totalDespesasMoradia = 0;
+                resumo.setTotalDespesasMoradia(despesas.get(i).getValor() + totalDespesasMoradia);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.TRANSPORTE) {
+                double totalDespesasTransporte = 0;
+                resumo.setTotalDespesasTransporte(despesas.get(i).getValor() + totalDespesasTransporte);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.EDUCAÇÃO) {
+                double totalDespesasEducacao = 0;
+                resumo.setTotalDespesasEducacao(despesas.get(i).getValor() + totalDespesasEducacao);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.LAZER) {
+                double totalDespesasLazer = 0;
+                resumo.setTotalDespesasLazer(despesas.get(i).getValor() + totalDespesasLazer);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.IMPREVISTOS) {
+                double totalDespesasImprevistos = 0;
+                resumo.setTotalDespesasImprevistos(despesas.get(i).getValor() + totalDespesasImprevistos);
+            }
+            if (despesas.get(i).getCategoria() == Categoria.OUTRAS) {
+                double totalDespesasOutras = 0;
+                resumo.setTotalDespesasOutras(despesas.get(i).getValor() + totalDespesasOutras);
+            }
         }
-        Resumo resumo = new Resumo();
+
         resumo.setTotalDespesas(totalDespesas);
 
         List<Receita> receitas = receitaRepository.findByAnoAndMes(startDate, endDate);
